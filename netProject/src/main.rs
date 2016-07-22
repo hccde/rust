933863c228b22b08809a8fecd14dba5c63f903ae
hccde <1076663958@qq.com>
2016-07-22 15:14:03 +0800

    最简单的rust http服务器

--- netProject/src/main.rs
+++ netProject/src/main.rs
@@ -2,17 +2,27 @@ use std::net::{TcpListener, TcpStream,Shutdown};
 use std::thread;
 use std::io::prelude::*;
 use std::fmt;
-
+use std::io::BufWriter;
+use std::u8;
+use std::str::*;
 fn main(){
+
 let listener = TcpListener::bind("127.0.0.1:5234").unwrap();
 
 fn handle_client(mut stream: TcpStream) {
 	println!("someone connect");
 	let mut str = String::new();
-	let mut  buf = vec![];
-	buf.write_all(b"HTTP/1.1 200 OK\r\n").unwrap();
-	stream.write(&buf);
-	stream.write_fmt(format_args!("hello admos"));//发送回去
+    let bufstr = "HTTP/1.1 201 OKs\r\nServer: bfe/1.0.8.14\r\nContent-Type: text/html;charset=utf-8\r\nContent-Length=100\r\n \r\n";
+    let mut vvv:Vec<u8> = vec![];
+    for b in bufstr.as_bytes() {
+            &vvv.push(*b);
+    }
+
+    println!("{:?}",from_utf8(&vvv).unwrap());
+	stream.write(&vvv);
+	stream.write_fmt(format_args!("\r\n<html><body>hello admos</body></html>"));//发送回去
+    stream.flush();
+    stream.shutdown(Shutdown::Write);
     println!("{:?}",stream.read_to_string(&mut str).unwrap());
     println!("{:?}", &str);
 
